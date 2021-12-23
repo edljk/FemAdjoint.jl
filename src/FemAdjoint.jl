b08477b1b02e8bbc02c2e6302ca03732b99b11ec
Edouard <edouard.oudet@imag.fr>
2021-12-23 07:51:41 +0100

    start Jacobian

--- src/FemAdjoint.jl
+++ src/FemAdjoint.jl
@@ -42,7 +42,9 @@ function costnormU(pin, t)
     return val
 end
 
-function ∇costnormU(pin, t)
+function ∇costnormU(pin, t;
+                    jacK::SparseMatrixCSC{Float64, Int64} = spzeros(0, 0),
+                    jacM::SparseMatrixCSC{Float64, Int64} = spzeros(0, 0))
     p = if typeof(pin) <: Vector
         reshape(pin, length(pin) ÷ 2, 2)
     elseif typeof(pin) <: Matrix
@@ -56,7 +58,20 @@ function ∇costnormU(pin, t)
     M = sparse(IM, JM, SM, np, np)
     F = M * ones(np)
     U = K \ M
-    # gradient 
+    # jacobian structures
+    if length(jacK) == 0
+        y = similar(FemAdjoint.assembKM_P12D(p, t)[1])
+        fs(y, x) = FemAdjoint.assembK_P12D_inplace(x, t, y)
+        sparsity_pattern = Symbolics.jacobian_sparsity(fs, y, p[:])
+        jacK = Float64.(sparse(sparsity_pattern))
+    end
+    if length(jacM) == 0
+        y = similar(FemAdjoint.assembKM_P12D(p, t)[2])
+        fs(y, x) = FemAdjoint.assembM_P12D_inplace(x, t, y)
+        sparsity_pattern = Symbolics.jacobian_sparsity(fs, y, p[:])
+        jacM = Float64.(sparse(sparsity_pattern))
+    end
+    # adjoint
     JFU = 2 * U 
     H = - K \ JFU
     val = sum(U[k] ^ 2 for k = 1:np)
